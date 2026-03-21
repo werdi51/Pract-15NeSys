beb50482cf2e01eddc77f2d882711c51dd465ab5
werdi51 <spotifig2@gmail.com>
2026-03-22 03:43:17 +0500

    What to write?

--- Pract 15/Pages/AdminPagesEdit/TagAddOrEdit.xaml.cs
+++ Pract 15/Pages/AdminPagesEdit/TagAddOrEdit.xaml.cs
@@ -1,28 +1,79 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Windows;
-    using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using Pract_15.Models;
+using Pract15;
 
-    namespace Pract_15.Pages.AdminPagesEdit
+namespace Pract_15.Pages.AdminPagesEdit
+{
+    public partial class TagAddOrEdit : Page, INotifyPropertyChanged
     {
-        /// <summary>
-        /// Логика взаимодействия для TagAddOrEdit.xaml
-        /// </summary>
-        public partial class TagAddOrEdit : Page
+        private Tag _currentTag;
+        private string _tagName;
+
+        public string TagName
         {
-            public TagAddOrEdit()
+            get => _tagName;
+            set { _tagName = value; OnPropertyChanged(); }
+        }
+
+        public TagAddOrEdit(Tag tag = null)
+        {
+            InitializeComponent();
+            _currentTag = tag;
+
+            if (_currentTag != null)
             {
-                InitializeComponent();
+                TagName = _currentTag.Name;
+            }
+
+            DataContext = this;
+        }
+
+        private async void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TagName))
+            {
+                MessageBox.Show("Введите название тега!");
+                return;
+            }
+
+            var context = DBService.Instance.Context;
+
+            try
+            {
+                if (_currentTag == null)
+                {
+                    var newTag = new Tag { Name = TagName };
+                    context.Tags.Add(newTag);
+                }
+                else
+                {
+                    _currentTag.Name = TagName;
+                    context.Tags.Update(_currentTag);
+                }
+
+                await context.SaveChangesAsync();
+                MessageBox.Show("Данные сохранены!");
+                NavigationService.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении: {ex.Message}");
             }
         }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
+}
